0a97c4e7e0511f65daea5a95fb10f6aaf233aedc
Praney Behl <praney.behl@myer.com.au>
2020-02-26 22:30:21 +1100

    build: setup jest for testing

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  testEnvironment: "jest-environment-jsdom-fifteen",
+  rootDir: __dirname,
+  collectCoverageFrom: [
+    "packages/components/src/**/*.{js,ts,tsx}",
+    "!**/*-test.{js,ts,tsx}"
+  ],
+  projects: ["<rootDir>/packages/*/jest*config.js"],
+  setupFilesAfterEnv: [
+    "raf/polyfill",
+    "@testing-library/jest-dom/extend-expect",
+    "@wordpress/jest-console",
+    "<rootDir>/jest.setup.js"
+  ]
+};
