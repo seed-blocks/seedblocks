f318de2bdb523fb6cc009f0465d035b8d451527a
Praney Behl <praneybehl@gmail.com>
2020-03-01 14:23:57 +1100

    fix: fix linting errors

--- packages/seedblocks-system/src/core/get.ts
+++ packages/seedblocks-system/src/core/get.ts
@@ -5,7 +5,7 @@ export function get(
 	p?: number,
 	undef?: unknown
 ): any {
-	const path = key && typeof key === 'string' ? key.split('.') : [key];
+	const path = key && typeof key === "string" ? key.split(".") : [key];
 	for (p = 0; p < path.length; p++) {
 		obj = obj ? (obj as any)[path[p]] : undef;
 	}
