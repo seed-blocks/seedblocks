5cb6203ebd56f20e89d60af271291f5c98b69e35
Praney Behl <praneybehl@gmail.com>
2020-03-07 10:17:25 +1100

    chore: add default space constants

--- packages/seedblocks-system/src/constants.ts
+++ packages/seedblocks-system/src/constants.ts
@@ -1,5 +1,6 @@
 const defaults = {
-	breakpoints: ["576px", "768px", "992px", "1200px"]
+	breakpoints: ["576px", "768px", "992px", "1200px"],
+	space: [0, 4, 8, 16, 32, 64, 128, 256, 512]
 };
 
 export { defaults };
