d2cfbbbe82254e888769c127d9bcfb3aefa0eb1d
Praney Behl <praneybehl@gmail.com>
2020-03-09 11:26:03 +1100

    chore: add fontSizes to constants

--- packages/seedblocks-system/src/constants.ts
+++ packages/seedblocks-system/src/constants.ts
@@ -1,6 +1,7 @@
 const defaults = {
 	breakpoints: ["576px", "768px", "992px", "1200px"],
-	space: [0, 4, 8, 16, 32, 64, 128, 256, 512]
+	space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+	fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72]
 };
 
 export { defaults };
