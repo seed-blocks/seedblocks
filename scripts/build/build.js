00e03f5471c2288d690402a8bd4ab37a83984ddf
Praney Behl <praneybehl@gmail.com>
2020-03-09 21:08:31 +1100

    feat: add build scripts

---
+++ scripts/build/build.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+const { join } = require("path");
+const spawn = require("cross-spawn");
+const {
+	makeProxies,
+	makeGitignore,
+	// makePlaygroundDeps,
+	cleanBuild,
+	hasTSConfig
+} = require("./utils");
+
+process.env.NODE_ENV = "production";
+
+if (process.argv.includes("--no-umd")) {
+	process.env.NO_UMD = true;
+}
+
+const cwd = process.cwd();
+
+cleanBuild(cwd);
+makeGitignore(cwd);
+// makePlaygroundDeps(cwd);
+makeProxies(cwd);
+
+if (hasTSConfig(cwd)) {
+	spawn.sync("tsc", ["--emitDeclarationOnly"], { stdio: "inherit" });
+}
+
+spawn.sync("rollup", ["-c", join(__dirname, "rollup.config.js")], {
+	stdio: "inherit"
+});
